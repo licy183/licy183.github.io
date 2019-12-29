dabda9ae65d236f183f2fed8be59a3d9a47caed1
Uchiha Kakashi <951156675@qq.com>
2019-12-29 12:46:22 +0800

    Site updated: 2019-12-29 12:46:20

---
+++ kihan/fortman/precache-manifest.db81c39aee86e871e0b6f08ba2fb3714.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5856a58f4494aa798f3582ea911d1c7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70a63950b94f488532b2",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e2e76177d13d6cb22096",
+    "url": "./static/js/2.d289be71.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.d289be71.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70a63950b94f488532b2",
+    "url": "./static/js/main.8a5bf890.chunk.js"
+  },
+  {
+    "revision": "9c2cc272e8203b889fe9",
+    "url": "./static/js/runtime-main.6a6b31e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- kihan/fortman/service-worker.js
+++ kihan/fortman/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bf87f348bfe3cf678ada6c03296fdf71.js"
+  "./precache-manifest.db81c39aee86e871e0b6f08ba2fb3714.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
